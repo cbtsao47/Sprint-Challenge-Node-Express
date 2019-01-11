168a9cfd0502df4cc73f9fa750cdb1a0bc8ee79c
wisdomtooth <cbtsao47@gmail.com>
2019-01-11 11:14:02 -0600

    add PUT and DELETE for projects

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -21,10 +21,49 @@ route.post("/add", async (req, res) => {
   const newProject = req.body;
   try {
     const result = await projectModel.insert(newProject);
-    res.status(201).json({ message: `Project has been created!` });
+    if (result) {
+      res.status(201).json({ message: `Project has been created!` });
+    } else {
+      res
+        .status(400)
+        .json({ message: `Please include both name and description` });
+    }
+  } catch (err) {
+    generalErr(err, res);
+  }
+});
+route.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const change = req.body;
+  try {
+    const project = await projectModel.get(id);
+    if (project) {
+      const result = await projectModel.update(id, change);
+      res.json({ message: `Project has been updated` });
+    } else {
+      res.status(404).json({ message: "Project not found!" });
+    }
   } catch (err) {
     generalErr(err, res);
   }
 });
 
+route.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await projectModel.get(id);
+    if (project) {
+      const count = await projectModel.remove(id);
+      res.json({
+        message: `${count} project with the title: "${
+          project.name
+        }"has been deleted`
+      });
+    } else {
+      res.status(404).json({ message: "Project not found!" });
+    }
+  } catch (err) {
+    generalErr(err, res);
+  }
+});
 module.exports = route;
