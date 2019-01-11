479d988664396f7daf103de1132b1ffcc00fb0a9
wisdomtooth <cbtsao47@gmail.com>
2019-01-11 10:31:18 -0600

    set up server

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const server = express();
+
+const configureMiddleware = require("../config/middleware");
+
+configureMiddleware(server);
+
+module.exports = server;
